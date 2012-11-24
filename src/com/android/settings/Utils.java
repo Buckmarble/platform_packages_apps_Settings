b17e8fa7e0f53b079b52cd06f5b97b5776093d61
buckmarble <forrestmarble@gmail.com>
2012-11-24 07:08:56 -0500

    remainder of files needed for nav bar edits

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -54,6 +54,7 @@ import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.Profile;
 import android.provider.ContactsContract.RawContacts;
+import android.content.res.Configuration;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -64,7 +65,11 @@ import android.widget.TabWidget;
 
 import com.android.settings.users.ProfileUpdateReceiver;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
@@ -74,6 +79,7 @@ import java.util.Locale;
 
 public class Utils {
 
+    private static final String TAG = "Utils";
     /**
      * Set the preference's title to the matching activity's label.
      */
@@ -469,6 +475,50 @@ public class Utils {
         } else {
             return R.string.tether_settings_title_bluetooth;
         }
+    }
+    public static boolean fileExists(String filename) {
+            return new File(filename).exists();
+        }
+
+    public static String fileReadOneLine(String fname) {
+	BufferedReader br;
+        String line = null;
+
+	try {
+	    br = new BufferedReader(new FileReader(fname), 512);
+	    try {
+		 line = br.readLine();
+	    } finally {
+		br.close();
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "IO Exception when reading /sys/ file", e);
+            }
+            return line;
+    }
+
+    public static boolean fileWriteOneLine(String fname, String value) {
+        try {
+            FileWriter fw = new FileWriter(fname);
+            try {
+                fw.write(value);
+            } finally {
+                fw.close();
+            }
+        } catch (IOException e) {
+            String Error = "Error writing to " + fname + ". Exception: ";
+            Log.e(TAG, Error, e);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isScreenLarge() {
+        final int screenSize = Resources.getSystem().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+        boolean isScreenLarge = screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+            screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE;
+        return isScreenLarge;
     }
 
     /* Used by UserSettings as well. Call this on a non-ui thread. */
