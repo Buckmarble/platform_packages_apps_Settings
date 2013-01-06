27a0ea802e2372b5fcd1dd9ad84628d111593690
buckmarble <forrestmarble@gmail.com>
2013-01-06 18:30:25 -0500

    Revert "still fixing"
    This reverts commit 3f179da3eaaea3c0e15e331fdddbdec523393de2.

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -90,6 +90,7 @@ public class SystemSettings extends SettingsPreferenceFragment {
         if (removeNavbar) {
             getPreferenceScreen().removePreference(findPreference(KEY_NAVIGATION_BAR));
         }
+    }
 
         mPowerButtonTorch = (CheckBoxPreference) findPreference(KEY_POWER_BUTTON_TORCH);
         if (torchSupported()) {
@@ -99,7 +100,6 @@ public class SystemSettings extends SettingsPreferenceFragment {
         } else {
             getPreferenceScreen().removePreference(mPowerButtonTorch);
         }
-     }
 
     @Override
     public void onResume() {
