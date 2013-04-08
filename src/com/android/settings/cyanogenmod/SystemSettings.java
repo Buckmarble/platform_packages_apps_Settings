509e0b8f8dc79cd4ff79686647baa78a18fbe684
buckmarble <forrestmarble@gmail.com>
2013-04-07 21:04:00 -0400

    missing import

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -30,6 +30,7 @@ import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
